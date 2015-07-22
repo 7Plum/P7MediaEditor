b55d7199e63af31ef2bd912870a498d4e53f1adc
kingiol <kingxiaokang@gmail.com>
2015-07-22 18:57:54 +0800

    update

---
+++ P7MediaEditor/P7MediaEditor.h
@@ -0,0 +1,19 @@
+//
+//  P7MediaEditor.h
+//  P7MediaEditor
+//
+//  Created by Kingiol on 15/7/12.
+//  Copyright © 2015年 Kingiol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for P7MediaEditor.
+FOUNDATION_EXPORT double P7MediaEditorVersionNumber;
+
+//! Project version string for P7MediaEditor.
+FOUNDATION_EXPORT const unsigned char P7MediaEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <P7MediaEditor/PublicHeader.h>
+
+
---
+++ P7MediaEditor/P7MediaEditor.swift
@@ -0,0 +1,9 @@
+//
+//  P7MediaEditor.swift
+//  P7MediaEditor
+//
+//  Created by Kingiol on 15/7/15.
+//  Copyright © 2015年 Kingiol. All rights reserved.
+//
+
+import Foundation
